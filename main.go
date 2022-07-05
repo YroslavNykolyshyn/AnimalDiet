840646cc623a1cd86df680c62f262f374d2a4dea
YaroslavNykolyshyn <yaroslav.nyko@gmail.com>
2022-07-05 16:39:28 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+type avgWeight interface {
+	getAvgWeight() int
+}
+
+func FeedAmount(a avgWeight, weight int) int {
+	fa := a.getAvgWeight() * weight
+	return fa
+}
+
+func totalFeedAmount(feedAmCow, feedAmCat, feedAmDog int) int {
+	feedAm := feedAmDog + feedAmCow + feedAmCat
+	return feedAm
+}
+
+func main() {
+	dog := Dog{
+		Name:       "Bob",
+		Weight:     5,
+		FeedAmount: 10,
+	}
+
+	var awDog avgWeight
+	awDog = dog
+	feedAmDog := FeedAmount(awDog, dog.Weight)
+	fmt.Printf("Dog name: %s"+
+		" Amount of food to feed a dog: %d \n",
+		dog.Name, feedAmDog)
+
+	cat := Cat{
+		Name:       "Juli",
+		Weight:     3,
+		FeedAmount: 7,
+	}
+	var awCat avgWeight
+	awCat = cat
+	feedAmCat := FeedAmount(awCat, cat.Weight)
+	fmt.Printf("Cat name: %s"+
+		" Amount of food to feed a cat: %d \n",
+		cat.Name, feedAmCat)
+
+	cow := Cow{
+		Name:       "Star",
+		Weight:     100,
+		FeedAmount: 25,
+	}
+	var awCow avgWeight
+	awCow = cow
+	feedAmCow := FeedAmount(awCow, cow.Weight)
+	fmt.Printf("Cow name: %s"+
+		" Amount of food to feed a cow: %d \n",
+		cow.Name, feedAmCow)
+
+	fmt.Printf("Total amount of feed: %d", totalFeedAmount(feedAmCow, feedAmCat, feedAmDog))
+
+}
---
+++ storage.go
@@ -0,0 +1,29 @@
+package main
+
+type Dog struct {
+	Name       string
+	Weight     int
+	FeedAmount int
+}
+type Cat struct {
+	Name       string
+	Weight     int
+	FeedAmount int
+}
+type Cow struct {
+	Name       string
+	Weight     int
+	FeedAmount int
+}
+
+func (d Dog) getAvgWeight() int {
+	return d.FeedAmount
+}
+
+func (c Cat) getAvgWeight() int {
+	return c.FeedAmount
+}
+
+func (co Cow) getAvgWeight() int {
+	return co.FeedAmount
+}
